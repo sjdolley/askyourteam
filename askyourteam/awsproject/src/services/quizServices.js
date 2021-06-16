c1e12c6b0c62bb867ba05cc5c83ef9ec64ab9ba5
ANotley <alicianotley@yahoo.co.nz>
2021-06-16 16:16:23 +1200

    remove old proxy packages, update dashboard GET

--- askyourteam/awsproject/src/services/quizServices.js
+++ askyourteam/awsproject/src/services/quizServices.js
@@ -1,33 +1,24 @@
-import useToken from '../useToken';
-
 //Any actions that occur on the /quizzes endpoint.
-export function getUserEmail(useToken) {
-    return fetch('/dev/me', {
+export function getQuizzes() {
+    //const url = 'https://bzj0anqy3l.execute-api.us-east-1.amazonaws.com/dev/getAllQuizByEmail' + '?' + qs.stringify({ email: 'example@gmail.com' });
+    const url = 'https://bzj0anqy3l.execute-api.us-east-1.amazonaws.com/dev/getAllQuizByEmail' + '?email=example@gmail.com';
+    return fetch(url , {
         method: 'GET',
         headers: {
-            'Content-Type': 'application/json'
-        }, body: useToken
-    })
-    .then(data => data.json())
-}
-
-export function getQuizzes(userEmail) {
-    return fetch('/dev/getAllQuizByEmail', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        }, body: userEmail
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer '+ sessionStorage.getItem('token')
+        }
     })
     .then(data => data.json())
 }
 
 export function getQuizByName() {
-    return fetch('/dev/getQuizByName')
+    return fetch('https://bzj0anqy3l.execute-api.us-east-1.amazonaws.com/dev/getQuizByName' )
         .then(data => data.json())
 }
 
 export function setQuizzes(newQuiz) { 
-    return fetch('/dev/createQuiz', {
+    return fetch('https://bzj0anqy3l.execute-api.us-east-1.amazonaws.com/dev/createQuiz', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
